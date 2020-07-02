7bc6a070a1eb2a65b49ae41e7da057c189efccfc
NafeeJ <nafeejan2@gmail.com>
2020-07-02 02:14:15 -0400

    Directory changes

---
+++ Assets/Scripts/FumoController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FumoController : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D fumoRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fumoRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            fumoRigidbody.AddForce(new Vector2(movementSpeed, 0f), ForceMode2D.Force);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            fumoRigidbody.AddForce(new Vector2(-movementSpeed, 0f), ForceMode2D.Force);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            fumoRigidbody.AddForce(new Vector2(0f, movementSpeed), ForceMode2D.Force);
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0f)
+        {
+            fumoRigidbody.AddForce(new Vector2(0f, -movementSpeed), ForceMode2D.Force);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnFumo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFumo : MonoBehaviour
+{
+
+    public FumoController cirno;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SpawnCirno()
+    {
+        Instantiate(cirno, new Vector3(0f, 0f, 0f), Quaternion.identity);
+    }
+
+    public void Clear()
+    {
+        FumoController[] fumos = FindObjectsOfType<FumoController>();
+
+        foreach (FumoController fumo in fumos)
+        {
+            Destroy(fumo.gameObject); // :(
+        }
+    }
+}
